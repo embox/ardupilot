3ee100ff30f1af509255d4821687f0427a9e64d7
Anton Bondarev <anton.bondarev2310@gmail.com>
2026-08-26 12:05:17 +0300

    templates: Add embox_k1921vg1t_arducopter

---
+++ ArdupilotEmbox/templates/embox_k1921vg1t_arducopter/board.conf.h
@@ -0,0 +1,18 @@
+#include <k1921vg1t_flight.conf.h>
+//#include <niiet_k1921vg1t_dev.conf.h>
+
+CONFIG {
+	uarts[0].status = ENABLED;
+	uarts[1].status = ENABLED;
+	uarts[2].status = ENABLED;
+	uarts[3].status = ENABLED;
+	uarts[4].status = ENABLED;
+
+	spis[0].status = ENABLED;
+	spis[1].status = ENABLED;
+	spis[2].status = ENABLED;
+	spis[3].status = ENABLED;
+
+	i2cs[0].status = ENABLED;
+	i2cs[1].status = ENABLED;
+}
